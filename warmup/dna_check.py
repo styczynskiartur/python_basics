26a09d94f77ded5f24a1ff00afd264b4aeac5534
styczynskiartur <styczynski.artur@gmail.com>
2019-07-07 12:38:39 +0200

    Change name hamming

--- warmup/dna_check.py
+++ warmup/dna_check.py
@@ -1,4 +1,4 @@
-def hamming_metric(father, child):
+def hamming(father, child):
     distance = 0
     for c in zip(child, father):
         cn, fn = c
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     second_dna = input("2nd candidate DNA: ")
     child_dna = input("child DNA: ")
 
-    first_distance = hamming_metric(first_dna, child_dna)
-    second_distance = hamming_metric(second_dna, child_dna)
+    first_distance = hamming(first_dna, child_dna)
+    second_distance = hamming(second_dna, child_dna)
     if first_distance > second_distance:
         print("Second candidate is a father")
     elif first_distance == second_distance:
